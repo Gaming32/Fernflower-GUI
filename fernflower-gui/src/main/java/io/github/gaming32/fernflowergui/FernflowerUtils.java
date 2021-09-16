82219fff2de22cb3843a004aa322275c97711976
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2021-09-16 14:36:09 -0500

    Create FernflowerUtils

---
+++ fernflower-gui/src/main/java/io/github/gaming32/fernflowergui/FernflowerUtils.java
@@ -0,0 +1,39 @@
+package io.github.gaming32.fernflowergui;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Map;
+
+import org.jetbrains.java.decompiler.main.decompiler.ConsoleDecompiler;
+import org.jetbrains.java.decompiler.main.decompiler.PrintStreamLogger;
+
+public class FernflowerUtils {
+    public static void decompile(File from, File to) {
+        decompile(new File[] {from}, to, Map.of());
+    }
+
+    public static void decompile(File[] sources, File to) {
+        decompile(sources, to, Map.of());
+    }
+
+    public static void decompile(File from, File to, Map<String, Object> options) {
+        decompile(new File[] {from}, to, options);
+    }
+
+    public static void decompile(File[] sources, File to, Map<String, Object> options) {
+        LibraryDecompiler decompiler = new LibraryDecompiler(to, options);
+        decompile(decompiler, sources, to, options);
+    }
+
+    public static <T extends ConsoleDecompiler> void decompile(T decompiler, File[] sources, File to, Map<String, Object> options) {
+        Arrays.stream(sources).forEach(decompiler::addSource);
+        decompiler.decompileContext();
+    }
+
+    // Publicize constructor
+    protected static class LibraryDecompiler extends ConsoleDecompiler {
+        public LibraryDecompiler(File destination, Map<String, Object> options) {
+            super(destination, options, new PrintStreamLogger(System.out));
+        }
+    }
+}
